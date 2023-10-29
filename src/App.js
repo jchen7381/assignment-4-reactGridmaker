ea2f56ae810e5cfe5a2e63f36732297af267db20
Ellahi <ellahinator02@gmail.com>
2023-10-29 15:12:20 -0400

    add a div wrapper around color picker button

--- src/App.js
+++ src/App.js
@@ -74,8 +74,10 @@ export default function App() {
         <button onClick={addColumn}>Add Column</button>
         <button onClick={removeRow}>Remove Row</button>
         <button onClick={removeColumn}>Remove Column</button>
-        <input type="color" onInput={changeColor} />
-      </div>
+        <div className="color-button-wrapper">
+            <button id="colorPickerButton" className="color-button">Pick Color</button>
+            <input id="colorPicker" type="color" className="hidden-color-picker" onInput={changeColor} />
+        </div>      </div>
       <Grid grid={grid} onClick={onClickCell} />
     </div>
   );
